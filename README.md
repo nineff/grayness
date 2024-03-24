80124f033fc40c1c11cf53d52c95336bdf4d1fb3
Niko <nineff@gmail.com>
2024-03-24 22:51:55 +0100

    basic running prototype

--- README.md
+++ README.md
@@ -1,1 +1,143 @@
 # GrayNess
+
+This is a proof of concept typst wasm-plugin to allow simple image editing functions from within typst, written in Rust.
+It uses the [wasm-minimal-protocol](https://github.com/astrale-sharp/wasm-minimal-protocol) crate to define the plugin functions. The image editing functionality is provided by the [image](https://crates.io/crates/image) crate.
+
+## Usage
+
+This plugin provides the following functions:
+
+- `blur(imagebytes, sigma)`: performs a Gaussian blur on the image. Sigma is a measure of how much to blur by. *Warning: This operation is SLOW*
+  Example usage:
+
+  ```typst
+  #let pl = plugin("gray_ness.wasm")
+  #image.decode(
+    pl.blur(
+      read("example.jpg",encoding: none), //read raw bytes from file
+      bytes("10") //specify bluring amount. Integers need to be converted to bytes from string
+    )
+  )
+
+  ```
+
+- `convert(imagebytes)`: used to display filetypes not directly supported by typst such as WebP by converting to PNG internally
+  Supported filetypes:
+  - Bmp
+  - Dds
+  - Farbfeld
+  - Gif
+  - Hdr
+  - Ico
+  - Jpeg
+  - OpenExr
+  - Png
+  - Pnm
+  - Qoi
+  - Tga
+  - Tiff
+  - WebP
+
+Example usage:
+
+```typst
+#let pl = plugin("gray_ness.wasm")
+#image.decode(
+  pl.convert(read("example.webp",encoding: none))
+)
+```
+
+- `crop(imagebytes, startx, starty, width, height)`: crop the image starting from the given x,y coordinates (top left corner) to the specified width and height from there. All values are in pixels
+  Example Usage:
+
+  ```typst
+  #let pl = plugin("gray_ness.wasm")
+  #image.decode(
+    pl.crop(
+      read("example.jpg",encoding: none), //read raw bytes from file
+      bytes("10"), //start x location in pixels
+      bytes("20"), //start y location in pixels
+      bytes("100"), //width in pixels
+      bytes("200") //heigth in pixels
+    )
+  )
+
+- `grayscale(imagebytes)`: turn the image into a black-and white version of itself
+  Example Usage:
+
+  ```typst
+  #let pl = plugin("gray_ness.wasm")
+  #image.decode(
+    pl.grayscale(
+      read("example.jpg",encoding: none)
+    )
+  )
+
+- `rotate90(imagebytes)`: rotate the image by 90° clockwise
+  Example Usage:
+
+  ```typst
+  #let pl = plugin("gray_ness.wasm")
+  #image.decode(
+    pl.rotate90(
+      read("example.jpg",encoding: none)
+    )
+  )
+
+- `rotate180(imagebytes)`: rotate the image by 180°
+  Example Usage:
+
+  ```typst
+  #let pl = plugin("gray_ness.wasm")
+  #image.decode(
+    pl.rotate180(
+      read("example.jpg",encoding: none)
+    )
+  )
+
+- `rotate270(imagebytes)`: rotate the image by 270° clockwise
+  Example Usage:
+
+  ```typst
+  #let pl = plugin("gray_ness.wasm")
+  #image.decode(
+    pl.rotate90(
+      read("example.jpg",encoding: none)
+    )
+  )
+
+- `flipv(imagebytes)`: flip the image vertically
+  Example Usage:
+
+  ```typst
+  #let pl = plugin("gray_ness.wasm")
+  #image.decode(
+    pl.flipv(
+      read("example.jpg",encoding: none)
+    )
+  )
+
+- `fliph(imagebytes)`: flip the image horizontally
+  Example Usage:
+
+  ```typst
+  #let pl = plugin("gray_ness.wasm")
+  #image.decode(
+    pl.fliph(
+      read("example.jpg",encoding: none)
+    )
+  )
+
+## Compile
+
+To compile this plugin, you need to have a working [Rust toolchain](https://www.rust-lang.org/). The Rust Version should be 1.76, I could not get it to work with 1.77. Then you need to install the `wasm32-unknown-unknown` target:
+
+```sh
+rustup target add wasm32-unknown-unknown
+```
+
+Then, build the crate with this target:
+
+```sh
+cargo build --release --target wasm32-unknown-unknown
+```
---
+++ src/lib.rs
@@ -0,0 +1,215 @@
+use image::{io::Reader as ImageReader, DynamicImage, ImageFormat};
+use std::{io::Cursor, str::from_utf8};
+use wasm_minimal_protocol::*;
+
+initiate_protocol!();
+
+#[wasm_func]
+pub fn grayscale(image_bytes: &[u8]) -> Result<Vec<u8>, String> {
+    let (img, mut format) = get_decoded_image_from_bytes(image_bytes)?;
+    let res = img.grayscale();
+
+    match format {
+        ImageFormat::Png | ImageFormat::Jpeg | ImageFormat::Gif => { // Do nothing
+        }
+        _ => {
+            format = ImageFormat::Png;
+        }
+    }
+
+    let mut bytes: Vec<u8> = Vec::new();
+    res.write_to(&mut Cursor::new(&mut bytes), format)
+        .map_err(|e| format!("Could not write image bytes to buffer: {e:?}"))?;
+
+    Ok(bytes)
+}
+
+#[wasm_func]
+pub fn convert(image_bytes: &[u8]) -> Result<Vec<u8>, String> {
+    let (img, mut format) = get_decoded_image_from_bytes(image_bytes)?;
+    
+    match format {
+        ImageFormat::Png | ImageFormat::Jpeg | ImageFormat::Gif => { // Do nothing
+        }
+        _ => {
+            format = ImageFormat::Png;
+        }
+    }
+
+    let mut bytes: Vec<u8> = Vec::new();
+    img.write_to(&mut Cursor::new(&mut bytes), format)
+        .map_err(|e| format!("Could not write image bytes to buffer: {e:?}"))?;
+
+    Ok(bytes)
+}
+
+#[wasm_func]
+pub fn crop(
+    image_bytes: &[u8],
+    start_x: &[u8],
+    start_y: &[u8],
+    width: &[u8],
+    height: &[u8],
+) -> Result<Vec<u8>, String> {
+    let start_x = bytes_to_int(start_x)?;
+    let start_y = bytes_to_int(start_y)?;
+    let width = bytes_to_int(width)?;
+    let height = bytes_to_int(height)?;
+    let (mut img, mut format) = get_decoded_image_from_bytes(image_bytes)?;
+    let res = img.crop(start_x, start_y, width, height);
+
+    match format {
+        ImageFormat::Png | ImageFormat::Jpeg | ImageFormat::Gif => { // Do nothing
+        }
+        _ => {
+            format = ImageFormat::Png;
+        }
+    }
+
+    let mut bytes: Vec<u8> = Vec::new();
+    res.write_to(&mut Cursor::new(&mut bytes), format)
+        .map_err(|e| format!("Could not write image bytes to buffer: {e:?}"))?;
+
+    Ok(bytes)
+}
+
+#[wasm_func]
+pub fn blur(image_bytes: &[u8], sigma: &[u8]) -> Result<Vec<u8>, String> {
+    let (img, mut format) = get_decoded_image_from_bytes(image_bytes)?;
+    let sigma = bytes_to_int(sigma)?;
+    let res = img.blur(sigma);
+
+    match format {
+        ImageFormat::Png | ImageFormat::Jpeg | ImageFormat::Gif => { // Do nothing
+        }
+        _ => {
+            format = ImageFormat::Png;
+        }
+    }
+
+    let mut bytes: Vec<u8> = Vec::new();
+    res.write_to(&mut Cursor::new(&mut bytes), format)
+        .map_err(|e| format!("Could not write image bytes to buffer: {e:?}"))?;
+
+    Ok(bytes)
+}
+
+#[wasm_func]
+pub fn flipv(image_bytes: &[u8]) -> Result<Vec<u8>, String> {
+    let (img, mut format) = get_decoded_image_from_bytes(image_bytes)?;
+    let res = img.flipv();
+
+    match format {
+        ImageFormat::Png | ImageFormat::Jpeg | ImageFormat::Gif => { // Do nothing
+        }
+        _ => {
+            format = ImageFormat::Png;
+        }
+    }
+
+    let mut bytes: Vec<u8> = Vec::new();
+    res.write_to(&mut Cursor::new(&mut bytes), format)
+        .map_err(|e| format!("Could not write image bytes to buffer: {e:?}"))?;
+
+    Ok(bytes)
+}
+
+#[wasm_func]
+pub fn fliph(image_bytes: &[u8]) -> Result<Vec<u8>, String> {
+    let (img, mut format) = get_decoded_image_from_bytes(image_bytes)?;
+    let res = img.fliph();
+
+    match format {
+        ImageFormat::Png | ImageFormat::Jpeg | ImageFormat::Gif => { // Do nothing
+        }
+        _ => {
+            format = ImageFormat::Png;
+        }
+    }
+    let mut bytes: Vec<u8> = Vec::new();
+    res.write_to(&mut Cursor::new(&mut bytes), format)
+        .map_err(|e| format!("Could not write image bytes to buffer: {e:?}"))?;
+
+    Ok(bytes)
+}
+
+#[wasm_func]
+pub fn rotate90(image_bytes: &[u8]) -> Result<Vec<u8>, String> {
+    let (img, mut format) = get_decoded_image_from_bytes(image_bytes)?;
+    let res = img.rotate90();
+
+    match format {
+        ImageFormat::Png | ImageFormat::Jpeg | ImageFormat::Gif => { // Do nothing
+        }
+        _ => {
+            format = ImageFormat::Png;
+        }
+    }
+    let mut bytes: Vec<u8> = Vec::new();
+    res.write_to(&mut Cursor::new(&mut bytes), format)
+        .map_err(|e| format!("Could not write image bytes to buffer: {e:?}"))?;
+
+    Ok(bytes)
+}
+
+#[wasm_func]
+pub fn rotate180(image_bytes: &[u8]) -> Result<Vec<u8>, String> {
+    let (img, mut format) = get_decoded_image_from_bytes(image_bytes)?;
+    let res = img.rotate180();
+
+    match format {
+        ImageFormat::Png | ImageFormat::Jpeg | ImageFormat::Gif => { // Do nothing
+        }
+        _ => {
+            format = ImageFormat::Png;
+        }
+    }
+    let mut bytes: Vec<u8> = Vec::new();
+    res.write_to(&mut Cursor::new(&mut bytes), format)
+        .map_err(|e| format!("Could not write image bytes to buffer: {e:?}"))?;
+
+    Ok(bytes)
+}
+
+#[wasm_func]
+pub fn rotate270(image_bytes: &[u8]) -> Result<Vec<u8>, String> {
+    let (img, mut format) = get_decoded_image_from_bytes(image_bytes)?;
+    let res = img.rotate270();
+
+    match format {
+        ImageFormat::Png | ImageFormat::Jpeg | ImageFormat::Gif => { // Do nothing
+        }
+        _ => {
+            format = ImageFormat::Png;
+        }
+    }
+    let mut bytes: Vec<u8> = Vec::new();
+    res.write_to(&mut Cursor::new(&mut bytes), format)
+        .map_err(|e| format!("Could not write image bytes to buffer: {e:?}"))?;
+
+    Ok(bytes)
+}
+
+fn bytes_to_int<T>(bytes: &[u8]) -> Result<T, String>
+where
+    T: std::str::FromStr + std::fmt::Debug,
+    T::Err: std::fmt::Debug,
+{
+    match from_utf8(bytes) {
+        Ok(input) => input
+            .parse()
+            .map_err(|e| format!("String could not be parsed as int: {e:?}")),
+        Err(e) => Err(format!("Invalid UTF8: {e:?}")),
+    }
+}
+
+fn get_decoded_image_from_bytes(bytes: &[u8]) -> Result<(DynamicImage, ImageFormat), String> {
+    let img_r = ImageReader::new(Cursor::new(bytes))
+        .with_guessed_format()
+        .map_err(|e| format!("Could not guess image format: {e:?}"))?;
+    let format = img_r.format().ok_or("No Format".to_string())?;
+    let decoded = img_r
+        .decode()
+        .map_err(|e| format!("Could not decode image data: {e:?}"))?;
+    Ok((decoded, format))
+}
