557e277b5ae02bec1aa4c86e1cfaddd80be76d62
Niko <nineff@gmail.com>
2024-03-25 23:25:08 +0100

    add transparency function

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use image::{io::Reader as ImageReader, DynamicImage, ImageFormat};
+use image::{io::Reader as ImageReader, DynamicImage, ImageFormat, Pixel};
 use std::{io::Cursor, str::from_utf8};
 use wasm_minimal_protocol::*;
 
@@ -27,7 +27,7 @@ pub fn grayscale(image_bytes: &[u8]) -> Result<Vec<u8>, String> {
 #[wasm_func]
 pub fn convert(image_bytes: &[u8]) -> Result<Vec<u8>, String> {
     let (img, mut format) = get_decoded_image_from_bytes(image_bytes)?;
-    
+
     match format {
         ImageFormat::Png | ImageFormat::Jpeg | ImageFormat::Gif => { // Do nothing
         }
@@ -185,6 +185,26 @@ pub fn rotate270(image_bytes: &[u8]) -> Result<Vec<u8>, String> {
     }
     let mut bytes: Vec<u8> = Vec::new();
     res.write_to(&mut Cursor::new(&mut bytes), format)
+        .map_err(|e| format!("Could not write image bytes to buffer: {e:?}"))?;
+
+    Ok(bytes)
+}
+
+#[wasm_func]
+pub fn transparency(image_bytes: &[u8], alpha: &[u8]) -> Result<Vec<u8>, String> {
+    let (img, _) = get_decoded_image_from_bytes(image_bytes)?;
+	let alpha = bytes_to_int(alpha)?;
+    let mut res = img.to_rgba8();
+
+    for y in 0..res.height() {
+        for x in 0..res.width() {
+            let pixel = res.get_pixel_mut(x, y);
+            pixel.apply_with_alpha(|ch| ch, |_| alpha);
+        }
+    }
+
+    let mut bytes: Vec<u8> = Vec::new();
+    res.write_to(&mut Cursor::new(&mut bytes), ImageFormat::Png)
         .map_err(|e| format!("Could not write image bytes to buffer: {e:?}"))?;
 
     Ok(bytes)
