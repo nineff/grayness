a8d726e6464fb84826b680babd5a27e7500301e9
Nikolai Neff-Sarnow <nineff@gmail.com>
2025-03-01 00:12:10 +0100

    added svg functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-use image::{io::Reader as ImageReader, DynamicImage, ImageFormat, Pixel};
-use std::{io::Cursor, str::from_utf8, u8};
+use image::{DynamicImage, ImageFormat, Pixel, io::Reader as ImageReader};
+use std::io::Cursor;
 use wasm_minimal_protocol::*;
 use xmltree::{Element, XMLNode};
 
@@ -33,7 +33,7 @@ fn svg_grayscale(image_bytes: &[u8]) -> Result<Vec<u8>, String> {
     let mut filter_elem = Element::new("filter");
     filter_elem
         .attributes
-        .insert("id".into(), "TypstGrayscaleFilter".into());
+        .insert("id".into(), "TypstSVGFilter".into());
     let mut colormatrix_elem = Element::new("feColorMatrix");
     colormatrix_elem
         .attributes
@@ -52,7 +52,7 @@ fn svg_grayscale(image_bytes: &[u8]) -> Result<Vec<u8>, String> {
     let mut group_element = Element::new("g");
     group_element
         .attributes
-        .insert("filter".into(), "url(#TypstGrayscaleFilter)".into());
+        .insert("filter".into(), "url(#TypstSVGFilter)".into());
 
     for child in svg_elem.children {
         if let XMLNode::Element(elem) = child {
@@ -99,7 +99,6 @@ pub fn crop(
     start_y: &[u8],
     width: &[u8],
     height: &[u8],
-    mode: &[u8],
 ) -> Result<Vec<u8>, String> {
     let start_x = u32::from_le_bytes(
         start_x
@@ -111,17 +110,18 @@ pub fn crop(
             .try_into()
             .map_err(|e| format!("could not convert bytes to int: {e:?}"))?,
     );
-    let mut width: f32 = utf8bytes_to_number(width)?;
-    let mut height: f32 = utf8bytes_to_number(height)?;
+    let width = u32::from_le_bytes(
+        width
+            .try_into()
+            .map_err(|e| format!("could not convert bytes to int: {e:?}"))?,
+    );
+    let height = u32::from_le_bytes(
+        height
+            .try_into()
+            .map_err(|e| format!("could not convert bytes to int: {e:?}"))?,
+    );
     let (mut img, mut format) = get_decoded_image_from_bytes(image_bytes)?;
-
-    if *mode.first().ok_or("Mode is not set".to_string())? == 1 {
-        let original_height = img.height();
-        let original_width = img.width();
-        width *= original_width as f32;
-        height *= original_height as f32;
-    }
-    let res = img.crop(start_x, start_y, width as _, height as _);
+    let res = img.crop(start_x, start_y, width, height);
 
     if !matches!(
         format,
@@ -138,9 +138,60 @@ pub fn crop(
 }
 
 #[wasm_func]
+fn svg_crop(
+    image_bytes: &[u8],
+    start_x: &[u8],
+    start_y: &[u8],
+    width: &[u8],
+    height: &[u8],
+) -> Result<Vec<u8>, String> {
+    let start_x = f32::from_le_bytes(
+        start_x
+            .try_into()
+            .map_err(|e| format!("could not convert bytes to float: {e:?}"))?,
+    );
+    let start_y = f32::from_le_bytes(
+        start_y
+            .try_into()
+            .map_err(|e| format!("could not convert bytes to float: {e:?}"))?,
+    );
+    let width = f32::from_le_bytes(
+        width
+            .try_into()
+            .map_err(|e| format!("could not convert bytes to float: {e:?}"))?,
+    );
+    let height = f32::from_le_bytes(
+        height
+            .try_into()
+            .map_err(|e| format!("could not convert bytes to float: {e:?}"))?,
+    );
+    let mut svg_elem =
+        Element::parse(image_bytes).map_err(|e| format!("Could not parse SVG data: {e:?}"))?;
+    if svg_elem.attributes.contains_key("viewBox") {
+        *svg_elem.attributes.get_mut("viewBox").unwrap() =
+            format!("{start_x} {start_y} {width} {height}");
+    } else {
+        svg_elem.attributes.insert(
+            "viewBox".to_string(),
+            format!("{start_x} {start_y} {width} {height}"),
+        );
+    }
+
+    let mut svg_output = Vec::new();
+    svg_elem
+        .write(&mut svg_output)
+        .map_err(|e| format!("Could not write SVG bytes: {e:?}"))?;
+    Ok(svg_output)
+}
+
+#[wasm_func]
 pub fn blur(image_bytes: &[u8], sigma: &[u8]) -> Result<Vec<u8>, String> {
     let (img, mut format) = get_decoded_image_from_bytes(image_bytes)?;
-    let sigma = utf8bytes_to_number(sigma)?;
+    let sigma = f32::from_le_bytes(
+        sigma
+            .try_into()
+            .map_err(|e| format!("could not convert bytes to float: {e:?}"))?,
+    );
     let res = img.blur(sigma);
 
     if !matches!(
@@ -158,9 +209,64 @@ pub fn blur(image_bytes: &[u8], sigma: &[u8]) -> Result<Vec<u8>, String> {
 }
 
 #[wasm_func]
+fn svg_blur(image_bytes: &[u8], sigma: &[u8]) -> Result<Vec<u8>, String> {
+    let mut svg_elem =
+        Element::parse(image_bytes).map_err(|e| format!("Could not parse SVG data: {e:?}"))?;
+
+    let sigma = f32::from_le_bytes(
+        sigma
+            .try_into()
+            .map_err(|e| format!("could not convert bytes to float: {e:?}"))?,
+    );
+
+    //create a gaussian blur filter
+    let mut filter_elem = Element::new("filter");
+    filter_elem
+        .attributes
+        .insert("id".into(), "TypstSVGFilter".into());
+    let mut fe_gaussian_blur = Element::new("feGaussianBlur");
+    fe_gaussian_blur
+        .attributes
+        .insert("stdDeviation".into(), format!("{sigma}"));
+
+    filter_elem
+        .children
+        .push(XMLNode::Element(fe_gaussian_blur));
+
+    //wrap all existing elements in a new group with the filter applied
+    let mut group_element = Element::new("g");
+    group_element
+        .attributes
+        .insert("filter".into(), "url(#TypstSVGFilter)".into());
+
+    for child in svg_elem.children {
+        if let XMLNode::Element(elem) = child {
+            group_element.children.push(XMLNode::Element(elem));
+        }
+    }
+
+    //add filter and replace existing children with new group
+    svg_elem.children = vec![
+        XMLNode::Element(filter_elem),
+        XMLNode::Element(group_element),
+    ];
+
+    let mut svg_output = Vec::new();
+
+    svg_elem
+        .write(&mut svg_output)
+        .map_err(|e| format!("Could not write SVG bytes: {e:?}"))?;
+    Ok(svg_output)
+}
+
+#[wasm_func]
 pub fn transparency(image_bytes: &[u8], alpha: &[u8]) -> Result<Vec<u8>, String> {
     let (img, _) = get_decoded_image_from_bytes(image_bytes)?;
-    let alpha = utf8bytes_to_number(alpha)?;
+    let alpha = u8::from_le_bytes(
+        alpha
+            .try_into()
+            .map_err(|e| format!("could not convert bytes to int: {e:?}"))?,
+    );
     let mut res = img.to_rgba8();
 
     for y in 0..res.height() {
@@ -175,6 +281,63 @@ pub fn transparency(image_bytes: &[u8], alpha: &[u8]) -> Result<Vec<u8>, String>
         .map_err(|e| format!("Could not write image bytes to buffer: {e:?}"))?;
 
     Ok(bytes)
+}
+
+#[wasm_func]
+fn svg_transparency(image_bytes: &[u8], alpha: &[u8]) -> Result<Vec<u8>, String> {
+    let mut svg_elem =
+        Element::parse(image_bytes).map_err(|e| format!("Could not parse SVG data: {e:?}"))?;
+
+    let alpha = f32::from_le_bytes(
+        alpha
+            .try_into()
+            .map_err(|e| format!("could not convert bytes to float: {e:?}"))?,
+    );
+
+    //create a component transfer filter for the alpha channel
+    let mut filter_elem = Element::new("filter");
+    filter_elem
+        .attributes
+        .insert("id".into(), "TypstSVGFilter".into());
+    let mut fe_component_transfer = Element::new("feComponentTransfer");
+    let mut fe_func_a = Element::new("feFuncA");
+    fe_func_a.attributes.insert("type".into(), "linear".into());
+    fe_func_a
+        .attributes
+        .insert("slope".into(), format!("{alpha}"));
+
+    fe_component_transfer
+        .children
+        .push(XMLNode::Element(fe_func_a));
+
+    filter_elem
+        .children
+        .push(XMLNode::Element(fe_component_transfer));
+
+    //wrap all existing elements in a new group with the filter applied
+    let mut group_element = Element::new("g");
+    group_element
+        .attributes
+        .insert("filter".into(), "url(#TypstSVGFilter)".into());
+
+    for child in svg_elem.children {
+        if let XMLNode::Element(elem) = child {
+            group_element.children.push(XMLNode::Element(elem));
+        }
+    }
+
+    //add filter and replace existing children with new group
+    svg_elem.children = vec![
+        XMLNode::Element(filter_elem),
+        XMLNode::Element(group_element),
+    ];
+
+    let mut svg_output = Vec::new();
+
+    svg_elem
+        .write(&mut svg_output)
+        .map_err(|e| format!("Could not write SVG bytes: {e:?}"))?;
+    Ok(svg_output)
 }
 
 #[wasm_func]
@@ -194,6 +357,76 @@ pub fn invert(image_bytes: &[u8]) -> Result<Vec<u8>, String> {
         .map_err(|e| format!("Could not write image bytes to buffer: {e:?}"))?;
 
     Ok(bytes)
+}
+
+#[wasm_func]
+fn svg_invert(image_bytes: &[u8]) -> Result<Vec<u8>, String> {
+    let mut svg_elem =
+        Element::parse(image_bytes).map_err(|e| format!("Could not parse SVG data: {e:?}"))?;
+
+    //create a component transfer filter for the RGB channels with invertion table
+    let mut filter_elem = Element::new("filter");
+    filter_elem
+        .attributes
+        .insert("id".into(), "TypstSVGFilter".into());
+    filter_elem
+        .attributes
+        .insert("style".into(), "color-interpolation-filters:sRGB".into());
+    let mut fe_component_transfer = Element::new("feComponentTransfer");
+    let mut fe_func_r = Element::new("feFuncR");
+    let mut fe_func_g = Element::new("feFuncG");
+    let mut fe_func_b = Element::new("feFuncB");
+    fe_func_r.attributes.insert("type".into(), "table".into());
+    fe_func_r
+        .attributes
+        .insert("tableValues".into(), "1 0".into());
+    fe_func_g.attributes.insert("type".into(), "table".into());
+    fe_func_g
+        .attributes
+        .insert("tableValues".into(), "1 0".into());
+    fe_func_b.attributes.insert("type".into(), "table".into());
+    fe_func_b
+        .attributes
+        .insert("tableValues".into(), "1 0".into());
+
+    fe_component_transfer
+        .children
+        .push(XMLNode::Element(fe_func_r));
+    fe_component_transfer
+        .children
+        .push(XMLNode::Element(fe_func_g));
+    fe_component_transfer
+        .children
+        .push(XMLNode::Element(fe_func_b));
+
+    filter_elem
+        .children
+        .push(XMLNode::Element(fe_component_transfer));
+
+    //wrap all existing elements in a new group with the filter applied
+    let mut group_element = Element::new("g");
+    group_element
+        .attributes
+        .insert("filter".into(), "url(#TypstSVGFilter)".into());
+
+    for child in svg_elem.children {
+        if let XMLNode::Element(elem) = child {
+            group_element.children.push(XMLNode::Element(elem));
+        }
+    }
+
+    //add filter and replace existing children with new group
+    svg_elem.children = vec![
+        XMLNode::Element(filter_elem),
+        XMLNode::Element(group_element),
+    ];
+
+    let mut svg_output = Vec::new();
+
+    svg_elem
+        .write(&mut svg_output)
+        .map_err(|e| format!("Could not write SVG bytes: {e:?}"))?;
+    Ok(svg_output)
 }
 
 #[wasm_func]
@@ -221,6 +454,85 @@ pub fn brighten(image_bytes: &[u8], amount: &[u8]) -> Result<Vec<u8>, String> {
 }
 
 #[wasm_func]
+fn svg_brighten(image_bytes: &[u8], amount: &[u8]) -> Result<Vec<u8>, String> {
+    let mut svg_elem =
+        Element::parse(image_bytes).map_err(|e| format!("Could not parse SVG data: {e:?}"))?;
+
+    let amount = f32::from_le_bytes(
+        amount
+            .try_into()
+            .map_err(|e| format!("could not convert bytes to float: {e:?}"))?,
+    );
+
+    //create a component transfer filter for the RGB channels
+    let mut filter_elem = Element::new("filter");
+    filter_elem
+        .attributes
+        .insert("id".into(), "TypstSVGFilter".into());
+    filter_elem
+        .attributes
+        .insert("style".into(), "color-interpolation-filters:sRGB".into());
+    let mut fe_component_transfer = Element::new("feComponentTransfer");
+    let mut fe_func_r = Element::new("feFuncR");
+    let mut fe_func_g = Element::new("feFuncG");
+    let mut fe_func_b = Element::new("feFuncB");
+    fe_func_r.attributes.insert("type".into(), "linear".into());
+    fe_func_r.attributes.insert("slope".into(), "1".into());
+    fe_func_r
+        .attributes
+        .insert("intercept".into(), format!("{amount}"));
+    fe_func_g.attributes.insert("type".into(), "linear".into());
+    fe_func_g.attributes.insert("slope".into(), "1".into());
+    fe_func_g
+        .attributes
+        .insert("intercept".into(), format!("{amount}"));
+    fe_func_b.attributes.insert("type".into(), "linear".into());
+    fe_func_b.attributes.insert("slope".into(), "1".into());
+    fe_func_b
+        .attributes
+        .insert("intercept".into(), format!("{amount}"));
+
+    fe_component_transfer
+        .children
+        .push(XMLNode::Element(fe_func_r));
+    fe_component_transfer
+        .children
+        .push(XMLNode::Element(fe_func_g));
+    fe_component_transfer
+        .children
+        .push(XMLNode::Element(fe_func_b));
+
+    filter_elem
+        .children
+        .push(XMLNode::Element(fe_component_transfer));
+
+    //wrap all existing elements in a new group with the filter applied
+    let mut group_element = Element::new("g");
+    group_element
+        .attributes
+        .insert("filter".into(), "url(#TypstSVGFilter)".into());
+
+    for child in svg_elem.children {
+        if let XMLNode::Element(elem) = child {
+            group_element.children.push(XMLNode::Element(elem));
+        }
+    }
+
+    //add filter and replace existing children with new group
+    svg_elem.children = vec![
+        XMLNode::Element(filter_elem),
+        XMLNode::Element(group_element),
+    ];
+
+    let mut svg_output = Vec::new();
+
+    svg_elem
+        .write(&mut svg_output)
+        .map_err(|e| format!("Could not write SVG bytes: {e:?}"))?;
+    Ok(svg_output)
+}
+
+#[wasm_func]
 pub fn huerotate(image_bytes: &[u8], amount: &[u8]) -> Result<Vec<u8>, String> {
     let (img, mut format) = get_decoded_image_from_bytes(image_bytes)?;
     let amount = i32::from_le_bytes(
@@ -244,17 +556,56 @@ pub fn huerotate(image_bytes: &[u8], amount: &[u8]) -> Result<Vec<u8>, String> {
     Ok(bytes)
 }
 
-fn utf8bytes_to_number<T>(bytes: &[u8]) -> Result<T, String>
-where
-    T: std::str::FromStr + std::fmt::Debug,
-    T::Err: std::fmt::Debug,
-{
-    match from_utf8(bytes) {
-        Ok(input) => input
-            .parse()
-            .map_err(|e| format!("String '{input}' could not be parsed as number: {e:?}")),
-        Err(e) => Err(format!("Invalid UTF8: {e:?}")),
+#[wasm_func]
+fn svg_huerotate(image_bytes: &[u8], amount: &[u8]) -> Result<Vec<u8>, String> {
+    let mut svg_elem =
+        Element::parse(image_bytes).map_err(|e| format!("Could not parse SVG data: {e:?}"))?;
+
+    let amount = f32::from_le_bytes(
+        amount
+            .try_into()
+            .map_err(|e| format!("could not convert bytes to float: {e:?}"))?,
+    );
+
+    //create a gaussian blur filter
+    let mut filter_elem = Element::new("filter");
+    filter_elem
+        .attributes
+        .insert("id".into(), "TypstSVGFilter".into());
+    let mut fe_color_matrix = Element::new("feColorMatrix ");
+    fe_color_matrix
+        .attributes
+        .insert("type".into(), "hueRotate".into());
+    fe_color_matrix
+        .attributes
+        .insert("values".into(), format!("{amount}deg"));
+
+    filter_elem.children.push(XMLNode::Element(fe_color_matrix));
+
+    //wrap all existing elements in a new group with the filter applied
+    let mut group_element = Element::new("g");
+    group_element
+        .attributes
+        .insert("filter".into(), "url(#TypstSVGFilter)".into());
+
+    for child in svg_elem.children {
+        if let XMLNode::Element(elem) = child {
+            group_element.children.push(XMLNode::Element(elem));
+        }
     }
+
+    //add filter and replace existing children with new group
+    svg_elem.children = vec![
+        XMLNode::Element(filter_elem),
+        XMLNode::Element(group_element),
+    ];
+
+    let mut svg_output = Vec::new();
+
+    svg_elem
+        .write(&mut svg_output)
+        .map_err(|e| format!("Could not write SVG bytes: {e:?}"))?;
+    Ok(svg_output)
 }
 
 fn get_decoded_image_from_bytes(bytes: &[u8]) -> Result<(DynamicImage, ImageFormat), String> {
